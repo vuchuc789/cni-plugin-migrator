8f6ed63743f3323fa6071e14ad22e0c2da685e05
Vũ Chức <vuchuc781999@gmail.com>
2024-06-19 09:12:16 +0700

    update script to set calico autodetection method

--- scripts/set_calico_autodetection_method.sh
+++ scripts/set_calico_autodetection_method.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl set env daemonset/calico-node -n kube-system IP_AUTODETECTION_METHOD=kubernetes-internal-ip
+kubectl set env daemonset/calico-node -n kube-system IP_AUTODETECTION_METHOD=cidr=172.16.120.0/24
